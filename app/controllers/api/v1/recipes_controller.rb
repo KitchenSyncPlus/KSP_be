8531ece87e2a6547b2562a7e5e304de12fb126d4
Lucas Colwell <colwellc37@gmail.com>
2023-01-06 13:20:49 -0700

    Update controller with recipe search

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,8 +1,25 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://api.edamam.com")
+    conn = Faraday.new(url: "https://api.edamam.com") do |faraday|
+      faraday.params['app_id'] =  ENV['edamam_app_id']
+      faraday.params['app_key'] =  ENV['edamam_app_key']
+    end
 
-    response = conn.get("/api/recipes/v2?type=public&beta=true&q=avocado&app_id=fc8a5789&app_key=3877bb1c95fcce6327d26aa97e4edf16")
+    response = conn.get("/api/recipes/v2?type=public&beta=true&q=avocado&")
+    #q will be params[:q] from search view
+    data = JSON.parse(response.body, symbolize_names: :true)
+    recipe_results = data[:hits]
+    recipe_results.each do |recipe|
+      ingredients_list = recipe[:recipe][:ingredients]
+      ingredients_array = ingredients_list.map do |i|
+        i[:food]
+      end
+      ingredients_string = ingredients_array.join ", "
+      Recipe.create!(ingredients: ingredients_string, source: recipe[:recipe][:source], url: recipe[:recipe][:url])
+    end
+
+    # require "pry"; binding.pry
+
     render json: RecipeSerializer.new(Recipe.all)
   end
 end
