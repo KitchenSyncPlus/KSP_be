54c79d985381483d505fd6b161253f169eee9e1c
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-01-09 17:16:35 -0700

    Adds basic model validations, relationships and testing
    - added simplecov, shoulda-matchers and configured; testing 100% for models
    
    - [x] I have performed a self-review of my code
    - [x] I have added thorough tests.
    - [x] I have commented my code, if needed
    
    good
    
    Co-authored-by: Astrid Hecht <106942456+Astrid-Hecht@users.noreply.github.com>
    Co-authored-by: Sage Skaff <99838997+sage-skaff@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,3 +72,10 @@ VCR.configure do |config|
   config.filter_sensitive_data('<edamam_app_key>') { ENV.fetch('edamam_app_key')}
   config.configure_rspec_metadata!
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
