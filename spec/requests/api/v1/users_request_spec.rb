ed5f70f94db9b455ee5d5336ab502820f46ce1bb
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-01-09 21:32:08 -0700

    Exposes single user endpoint
    - [x] I have performed a self-review of my code
    - [x] I have added thorough tests.
    - [x] I have commented my code, if needed
    
    good
    
    Co-authored-by: Astrid Hecht <106942456+Astrid-Hecht@users.noreply.github.com>
    Co-authored-by: Kevin Ta <36166420+KevinT001@users.noreply.github.com>
    Co-authored-by: Sage Skaff <99838997+sage-skaff@users.noreply.github.com>

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -8,11 +8,28 @@ RSpec.describe 'Users', type: :request do
 
       parsed_response = JSON.parse(response.body, symbolize_names: true)
 
-      expect(parsed_response).not_to be_empty
+      expect(parsed_response).to_not be_empty
     end
 
-    xit 'creates a user' do
-      post '/api/v1/users'
+    it 'gets a single user' do
+      user1 = User.create!(name: Faker::Name.name, email: Faker::Internet.email)
+      user1.groups.create!(name: Faker::Color.color_name, about: Faker::Company.bs)
+
+      get "/api/v1/users/#{user1.id}"
+
+      user = JSON.parse(response.body, symbolize_names: true)
+      expect(response.status).to be(200)
+      expect(response.body).to_not be_empty
+      expect(user).to have_key(:data)
+      expect(user[:data]).to be_a Hash
+      expect(user[:data]).to have_key (:id)
+      expect(user[:data]).to have_key (:type)
+      expect(user[:data][:type]).to eq('user')
+      expect(user[:data]).to have_key (:attributes)
+      expect(user[:data][:attributes]).to have_key(:name)
+      expect(user[:data][:attributes]).to have_key(:email)
+      expect(user[:data][:attributes]).to have_key(:groups)
+      expect(user[:data][:attributes][:groups]).to be_an Array
     end
   end
 end
